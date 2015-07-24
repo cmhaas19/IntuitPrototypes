2c7484edb9f325f97e9c50afa1a6858856295e33
Chris Haas <chrismhaas@gmail.com>
2015-07-23 22:19:42 -0700

    Concept B and main landing page

--- app/prototypeApp.js
+++ app/prototypeApp.js
@@ -10,26 +10,15 @@
         .config(['$routeProvider', function ($routeProvider) {
 
             $routeProvider
-                .when("/concept/a/quickbooks", { 
-                    templateUrl: "/app/views/conceptA/quickbooks.html", 
-                    title: 'Concept A - Quickbooks', 
-                    bodyClass: 'concept-a quickbooks'
-                })
-                .when("/concept/a/mint", { 
-                    templateUrl: "/app/views/conceptA/mint.html", 
-                    title: 'Concept A - Mint', 
-                    bodyClass: 'concept-a mint'
-                })
-                .when("/concept/a/turbotax", { 
-                    templateUrl: "/app/views/conceptA/turbotax.html", 
-                    title: 'Concept A - TurboTax', 
-                    bodyClass: 'concept-a turbotax'
-                })
-                .when("/concept/b/quickbooks", { 
-                    templateUrl: "/app/views/conceptB/quickbooks.html", 
-                    title: 'Concept B - Quickbooks', 
-                    bodyClass: 'concept-b quickbooks'
-                })
+                .when("/concept/a/quickbooks", { templateUrl: "/app/views/conceptA/quickbooks.html", title: 'Concept A - Quickbooks', bodyClass: 'concept-a quickbooks' })
+                .when("/concept/a/mint", { templateUrl: "/app/views/conceptA/mint.html", title: 'Concept A - Mint', bodyClass: 'concept-a mint' })
+                .when("/concept/a/turbotax", { templateUrl: "/app/views/conceptA/turbotax.html", title: 'Concept A - TurboTax', bodyClass: 'concept-a turbotax' })
+
+                .when("/concept/b/intuit", { templateUrl: "/app/views/conceptB/intuit.html", title: 'Concept B - Intuit', bodyClass: 'concept-b intuit' })
+                .when("/concept/b/quickbooks", { templateUrl: "/app/views/conceptB/quickbooks.html", title: 'Concept B - Quickbooks', bodyClass: 'concept-b quickbooks' })
+                .when("/concept/b/turbotax", { templateUrl: "/app/views/conceptB/turbotax.html", title: 'Concept B - TurboTax', bodyClass: 'concept-b turbotax' })
+                .when("/concept/b/mint", { templateUrl: "/app/views/conceptB/mint.html", title: 'Concept B - Mint', bodyClass: 'concept-b mint' })
+                
                 .otherwise({ 
                     templateUrl: "/app/views/home.html", 
                     bodyClass: 'concept-home' 
